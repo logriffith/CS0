b49b5e87a493e2d0ca46fbb9876d928dda8dd331
Lew Griffith <Lew@Lews-MacBook-Pro.local>
2020-01-31 12:32:25 -0700

    Notes on Assignment 1

---
+++ notes/assignment1.py
@@ -0,0 +1,18 @@
+"""
+Assignment 1: A siimply hangman game steps
+-program displays various stages of hangman game
+
+By: Ram Basnet
+1/30/20
+
+"""
+print("Hey there what's your name? ")
+# TBD- read the name and store into a variable
+print('Hey ...') # FIXME
+print('The hangman game is under construction, maybe you will get to play it i a few weeks')
+print("this is what various stages of hangman game will look like")
+print()
+print("Stage 0")
+print("|-------")
+print("|\       |")
+#... fix the rest
