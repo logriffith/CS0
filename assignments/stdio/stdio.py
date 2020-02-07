2f1b8194687de45e0c450d1f81681395b9f7dfc1
Lew Griffith <Lew@Lews-MacBook-Pro.local>
2020-02-06 18:02:29 -0700

    assignment 1 file

---
+++ assignments/stdio/stdio.py
@@ -0,0 +1,8 @@
+"""
+Assignment 1: Standard Input and Output
+By: Lew Griffith
+
+This program is the Hangman Game.
+
+The task is to write a program that prints various stages/gallows of the Hangman Game
+"""
