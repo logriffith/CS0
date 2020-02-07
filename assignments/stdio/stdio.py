dc9523988648908649cdc0ad4367c871f755261c
Lew Griffith <Lew@Lews-MacBook-Pro.local>
2020-02-06 19:53:12 -0700

    assignment 1

--- assignments/stdio/stdio.py
+++ assignments/stdio/stdio.py
@@ -7,7 +7,6 @@ This program is the Hangman Game.
 The task is to write a program that prints various stages/gallows of the Hangman Game
 """
 print("Hello welcome to the Hangman Game!")
-
 name=input("What is your name? ")
-
-print("Hello" , name)
+print("Why Hello" , name,"!")
+print("The hangman game is under construction.\nMaybe you’ll get to play it in a few weeks.\nThis is what various stages of hangman game will look like…")
