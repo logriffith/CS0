67f500d8cbed1f27e66002faa69b9f1a35e06205
Lew Griffith <Lew@Lews-MacBook-Pro.local>
2020-02-28 21:10:54 -0700

    functions assignment

---
+++ assignments/functions/functions.py
@@ -0,0 +1,7 @@
+"""
+Assignment 3: A Basic Calculator using Functions and Automated Unit Testing
+By: Lew Griffith
+
+The task is to write a Python program that performs some arithmetic operations on two given numbers. These numbrers will be inputed by the user.
+
+"""
