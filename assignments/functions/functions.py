ca8e5baf21be8687d11cd963388c521609dfc8dc
Lew Griffith <Lew@Lews-MacBook-Pro.local>
2020-02-28 23:04:33 -0700

    updated functions assignment

--- assignments/functions/functions.py
+++ assignments/functions/functions.py
@@ -5,3 +5,50 @@ By: Lew Griffith
 The task is to write a Python program that performs some arithmetic operations on two given numbers. These numbrers will be inputed by the user.
 
 """
+
+import math
+
+
+def sum(a,b):
+    sum=a+b
+    return sum
+
+def multiply(a,b):
+    product=a*b
+    return product
+
+def divide(a,b):
+    if b!=0:
+        quotient=a/b
+        return quotient
+    else:
+        print("Error: Cannot Divide by Zero")
+
+def subtract(a,b):
+    difference=a-b
+    return difference
+
+def modular(a,b):
+    if b!=0:
+        mod=a%b
+        return mod
+    else: 
+        print("Error: Cannot Divide by Zero")
+
+def power(a,b):
+    if b==0:
+        return 1
+    else:
+        answer=a**b
+        return answer
+
+def square_root(a):
+    if a>=0:
+        root=math.sqrt(a)
+        return root
+    else:
+        print("Not a real number")
+
+
+
+
