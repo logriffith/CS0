fe9e08f234de17a1b43717d7b464c27abc8b9526
Lew Griffith <Lew@Lews-MacBook-Pro.local>
2020-03-07 15:29:05 -0700

    assignment 4 created

---
+++ assignments/conditional/conditional.py
@@ -0,0 +1,6 @@
+"""
+Assignment 4: Conditionals
+By: Lew Griffith
+
+The task is to write a Python program that computes certain values such as sum, product, max, min and average of any 5 given numbers. 
+"""
