82319d223757080205ff73c8c8f8ebbdd475ed08
Lew Griffith <Lew@Lews-MacBook-Pro.local>
2020-03-14 21:53:37 -0600

    updated assignment

--- assignments/loop/loop.py
+++ assignments/loop/loop.py
@@ -11,14 +11,21 @@ print("Hello, welcome to the Guess the Number Game!")
 print()
 name=input("What is your name? ")
 number=random.randint(1,20)
-print("Hello,", name, "it is nice to meet you. I am thinking of a number between 1 and 20. Care to take a guess what it is? You will will have 6 guesses.")
+print("Hello,", name, "it is nice to meet you. I am thinking of a whole number between 1 and 20. Care to take a guess what it is? You can have up to 6 guesses.")
+print(number)
 for i in range(1,7):
     guess=int(input("What is your guess? "))
-    if guess>number:
+    if i==6 and guess!=number:
+        if guess>number:
+            print("Your guess is too high. This makes 6 attempts. You lose the game.")
+        else:
+            print("Your guess is too low. This makes 6 attempts. You lose the game.")
+    elif guess>number:
         print("I am sorry, your guess is too high. Please take another guess.")
     elif guess<number:
         print("I am sorry, your guess is too low. Please take another guess.")
     elif guess==number:
         print("Congratulations,", name,"you win! You guessed the number in", i, "attempts.")
+        break
     
 
