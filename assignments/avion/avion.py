c5c32952163351d5770330401ec0c01167e6161f
Lew Griffith <Lew@Lews-MacBook-Pro.local>
2020-04-12 22:43:20 -0600

    updated

--- assignments/avion/avion.py
+++ assignments/avion/avion.py
@@ -8,8 +8,9 @@ The task is to solve a kattis problem called "Avion". USKOK agents are looking f
 # 1. Get the data from the user
 # 2. Store the data in variables
 # 3. Search for "FBI" in each string
-# 4. If found, output the row number on one line separated by a space each and in increasing order
-# 5. If not, output "HE GOT AWAY"
+# 4. If found, return the row number on one line separated by a space each and in increasing order. If not, return "HE GOT AWAY".
+# 5. Print the result
+# 6. Test its accuracy
 
 import sys
 
@@ -26,25 +27,27 @@ def findBlimp(b1,b2,b3,b4,b5):
     blimp=[b1,b2,b3,b4,b5]
     counter=0
     answer=''
+# Step 3
     for code in blimp:
         if code.find("FBI")>=0:
             answer+=str(blimp.index(code)+1)+' '
         else:
             counter+=1
+# Step 4 
     if counter==5:
         return "HE GOT AWAY!"
     else:
         return answer  
-
+# Step 6
 def test():
     assert findBlimp("FBIuej","FBIdjfk","8","ueu","FBIkdkf")=="1 2 5 "   
     assert findBlimp("mommy_dearest","superman","krypto","boombam","hi")=="HE GOT AWAY!"   
     assert findBlimp("hi","FBI11","FBI!","kdfkdjf", "graduation!!!!")=="2 3 "
     print("All test cases passed")
 
-#findBlimp(getBlimps())
 if len(sys.argv)==2 and sys.argv[1]=="test":
     test()
 else:
+# Step 5
     b1,b2,b3,b4,b5=getBlimps()
     print(findBlimp(b1,b2,b3,b4,b5))
