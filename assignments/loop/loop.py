1deaaaef18d32df638f7b4f247a0809376ecc923
Lew <Lew@Lews-MacBook-Pro.local>
2020-07-31 00:54:19 -0600

    updated number game

--- assignments/loop/loop.py
+++ assignments/loop/loop.py
@@ -6,47 +6,46 @@ The task is to write a Python program that will have the user play the typical g
 
 """
 import random 
-answer='y'
-game_played=0
-lost=0
-won=0
+answer = 'y'
+game_played = 0
+lost = 0
+won = 0
 print()
 print("Hello, welcome to the Guess the Number Game!")
 print()
-name=input("What is your name? ")
+name = input("What is your name? ")
 print()
 print("Hello,", name, "it is nice to meet you.")
-print()
-while answer=='y':
-    print("I am thinking of a whole number between 1 and 20. Care to take a guess what it is?\nYou can have up to 6 guesses.")
+while answer == 'y':
+    print("\nI am thinking of a whole number between 1 and 20. Care to take a guess what it is?\nYou can have up to 6 guesses.")
     print()
     number=random.randint(1,20)
     for i in range(1,7):
-        guess=int(input("What is your guess? "))
-        if i==6 and guess!=number:
-            if guess>number:
-                print("Your guess is too high. This makes 6 attempts. You lose the game. \nThe number was", number,'.')
+        guess = int(input("What is your guess? "))
+        if i == 6 and guess != number:
+            if guess > number:
+                print(f"Your guess is too high. This makes 6 attempts. You lose the game. \nThe number was {number}.")
                 print()
             else:
-                print("Your guess is too low. This makes 6 attempts. You lose the game. \nThe number was", number,'.')
+                print(f"Your guess is too low. This makes 6 attempts. You lose the game. \nThe number was {number}.")
                 print()
-            game_played+=1
-            lost+=1
-        elif guess>number:
+            game_played += 1
+            lost += 1
+        elif guess > number:
             print("I am sorry, but your guess is too high. Please make another guess.")
             print()
-        elif guess<number:
+        elif guess < number:
             print("I am sorry, but your guess is too low. Please make another guess.")
             print()
-        elif guess==number:
-            print("Congratulations,", name,"you win!!! The number is indeed", number, ".\nYou guessed the number in", i, "attempt(s).")
-            game_played+=1
-            won+=1
+        elif guess == number:
+            print("Congratulations, {} you win!!! The number is indeed {}.\nYou guessed the number in {} attempt(s).".format(name,number,i))
+            game_played += 1
+            won += 1
             print()
             break
-    answer=input("Would you like to play again? [y or n] ")
+    answer = input("Would you like to play again? [y or n] ")
 print()
 print("Here are your stats:")
 print("\tThe game was played", game_played, "time(s).")
 print("\tYou won the game", won, "time(s).")
-print("\tYou lost the game", lost, "time(s).")
+print("\tYou lost the game %d time(s)."%(lost))
